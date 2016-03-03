a3ae1f96641f6a6583fbfc48f22fd8211b1c9ce1
jresinas <jresinas@emergya.com>
2016-03-03 11:43:36 +0100

    Rename oauth callback name to avoid collision with other plugins

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RedmineApp::Application.routes.draw do
 	match '/cpm_user_capacity/edit/:id' => 'cpm_user_capacity#edit', via: [:post, :put, :patch]
 	match '/cpm_user_capacity/delete/:id' => 'cpm_user_capacity#delete', via: [:delete]
 	match '/cpm_management/get_filter_custom_field/:custom_field_id' => 'cpm_management#get_filter_custom_field', via: [:get]
-	get 'oauth2callback', :to => 'cpm_management#oauth_callback', :as => 'oauth_callback'
+	get 'oauth2callback_cpm', :to => 'cpm_management#oauth_callback', :as => 'oauth_callback'
 
 	match '/cpm_reports/:action' => 'cpm_reports', via: [:get, :post]
 	match '/cpm_reports/reports.:format' => 'cpm_reports#reports', via: [:get, :post]
